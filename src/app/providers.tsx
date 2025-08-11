5e84da481cd5c874bf95f8fe94df0ec477b301de
houssamotarid@gmail.com <houssam@collective.work>
2025-08-11 20:35:50 +0200

    ui: add remaining shadcn components for chat interface

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
-
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from '@/registry/new-york-v4/ui/toaster';
 
 export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SessionProvider>
-            <ThemeProvider attribute='class'>{children}</ThemeProvider>
+            <ThemeProvider attribute='class'>
+                {children}
+                <Toaster />
+            </ThemeProvider>
         </SessionProvider>
     );
 }
